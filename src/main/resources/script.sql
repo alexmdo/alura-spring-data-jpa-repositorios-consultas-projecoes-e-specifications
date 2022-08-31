71cb4911709574f11614fccac5a0cc39d4d54ac6
Alexandre Oliveira <alexmdo@gmail.com>
2022-08-30 21:56:13 -0300

    DB Load script

---
+++ src/main/resources/script.sql
@@ -0,0 +1,10 @@
+INSERT INTO alura.cargos (id, descricao) VALUES (1, 'DEVELOPER');
+INSERT INTO alura.cargos (id, descricao) VALUES (2, 'ENGINEER');
+INSERT INTO alura.cargos (id, descricao) VALUES (3, 'QA');
+
+INSERT INTO alura.funcionarios (id, cpf, data_contratacao, nome, salario, cargo_id) VALUES (1, '111111', '2022-08-29', 'ALEXANDRE', 1000, 1);
+INSERT INTO alura.funcionarios (id, cpf, data_contratacao, nome, salario, cargo_id) VALUES (2, '222222', '2022-07-12', 'AMANDA', 2000, 2);
+INSERT INTO alura.funcionarios (id, cpf, data_contratacao, nome, salario, cargo_id) VALUES (3, '333333', '2021-08-20', 'ISABELLA', 3000, 3);
+INSERT INTO alura.funcionarios (id, cpf, data_contratacao, nome, salario, cargo_id) VALUES (4, '444444', '2021-08-08', 'STEPHANIE', 4000, 1);
+INSERT INTO alura.funcionarios (id, cpf, data_contratacao, nome, salario, cargo_id) VALUES (5, '555555', '2022-12-21', 'NICK', 5000, 2);
+INSERT INTO alura.funcionarios (id, cpf, data_contratacao, nome, salario, cargo_id) VALUES (6, '666666', '2020-08-06', 'OREO', 6000, 3);
